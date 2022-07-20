909aba151930704f6508e2e323c8f3fe2535212f
senras <isenras@hotmail.com>
2022-07-20 02:21:04 -0300

    Comunicacion entre server y clients

--- models/server.js
+++ models/server.js
@@ -32,9 +32,15 @@ class Server {
 
 	sockets() {
 		this.io.on('connection', (socket) => {
-			console.log('Cliente conectado', socket.id);
 			socket.on('disconnect', () => {
-				console.log('Cliente desconectado', socket.id);
+				// console.log('Cliente desconectado', socket.id);
+			});
+
+			socket.on('enviar-mensaje', (payload, callback) => {
+				const id = 123456;
+				callback(id);
+
+				// this.io.emit('enviar-mensaje', payload);
 			});
 		});
 	}
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,15 +1,33 @@
 const lbOnline = document.querySelector('#lbOnline');
 const lbOffline = document.querySelector('#lbOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
 const socket = io();
 
 socket.on('connect', () => {
-	console.log('Conectado');
+	// console.log('Conectado');
 	lbOffline.style.display = 'none';
 	lbOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-	console.log('Desconectado');
+	// console.log('Desconectado');
 	lbOnline.style.display = 'none';
 	lbOffline.style.display = '';
+});
+
+socket.on('enviar-mensaje', (payload) => {
+	console.log(payload);
+});
+
+btnEnviar.addEventListener('click', () => {
+	const payload = {
+		mensaje: txtMensaje.value,
+		id: '123456',
+		fecha: new Date().getTime(),
+	};
+	socket.emit('enviar-mensaje', payload, (id) => {
+		console.log('Desde el server', id);
+	});
 });
